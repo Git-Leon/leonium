8a275ac2fabc592891c38a5b2570d59fef236a15
Leon Hunter <leonhunter@Leons-MacBook-Pro.local>
2019-04-30 10:35:35 -0400

    added static method `WebEntity.getByValue(WebElement)`

--- src/main/java/com/git_leon/leonium/browsertools/browserhandler/WebEntity.java
+++ src/main/java/com/git_leon/leonium/browsertools/browserhandler/WebEntity.java
@@ -95,4 +95,30 @@ public class WebEntity {
                 .replaceAll("\\[.*?\\] -> ", "")
                 .replaceAll("]", "");
     }
+
+    public static By getByValue(WebElement we) {
+        // By format = "[foundFrom] -> locator: term"
+        // see RemoteWebElement toString() implementation
+        String[] data = we.toString().split(" -> ")[1].replace("]", "").split(": ");
+        String locator = data[0];
+        String term = data[1];
+
+        switch (locator) {
+            case "xpath":
+                return By.xpath(term);
+            case "css selector":
+                return By.cssSelector(term);
+            case "id":
+                return By.id(term);
+            case "tag name":
+                return By.tagName(term);
+            case "name":
+                return By.name(term);
+            case "link text":
+                return By.linkText(term);
+            case "class name":
+                return By.className(term);
+        }
+        return (By) we;
+    }
 }
