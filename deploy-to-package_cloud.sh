4cae73aa8a4d06708fa1610d14538166ea62b175
git-leon <xleonhunter@gmail.com>
2021-06-03 09:39:30 -0400

    changed deploy script

--- deploy-to-package_cloud.sh
+++ deploy-to-package_cloud.sh
@@ -3,7 +3,7 @@ rm *jar
 rm dependency-reduced-pom.xml
 ls
 
-project_version=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
 git pull --all
+project_version=`mvn help:evaluate -Dexpression=project.version -q -DforceStdout`
 mvn package -Dmaven.test.skip=true
 package_cloud push git-leon/utils ./target/leonium-$project_version.jar --coordinates=com.github.git-leon:leonium:$project_version
