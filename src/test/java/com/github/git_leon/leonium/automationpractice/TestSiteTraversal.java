6c17a440fd72479879dc2899fd6bc77667d35626
Leon Hunter <xleonhunter@gmail.com>
2022-05-17 13:22:40 -0400

    updated testing; confirmed waiting is behaving correctly

--- src/test/java/com/github/git_leon/leonium/automationpractice/TestSiteTraversal.java
+++ src/test/java/com/github/git_leon/leonium/automationpractice/TestSiteTraversal.java
@@ -1,23 +1,20 @@
 package com.github.git_leon.leonium.automationpractice;
 
-import com.github.git_leon.leonium.DirectoryReference;
 import com.github.git_leon.leonium.automationpractice.webpages.HomePage;
 import com.github.git_leon.leonium.automationpractice.webpages.SearchResultPage;
 import com.github.git_leon.leonium.automationpractice.webpages.ShoppingCartSummaryPage;
 import com.github.git_leon.leonium.browsertools.browserhandler.core.BrowserHandlerInterface;
 import com.github.git_leon.leonium.browsertools.browserhandler.reporting.BrowserHandlerLayeredLogger;
 import com.github.git_leon.leonium.browsertools.factories.BrowserHandlerFactory;
-import com.github.git_leon.leonium.extentreporting.ExtentTestLogger;
 import com.github.git_leon.leonium.extentreporting.ExtentTestLoggerFactory;
 import com.github.git_leon.leonium.extentreporting.ExtentTestLoggerFactoryManager;
 import com.github.git_leon.leonium.extentreporting.ExtentTestLoggerInterface;
-import com.github.git_leon.stringutils.StringUtils;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 
-import java.time.LocalDateTime;
+import java.io.File;
 import java.util.logging.Level;
 
 /**
@@ -28,7 +25,7 @@ public class TestSiteTraversal {
     private ExtentTestLoggerFactory extentTestLoggerFactory;
     private BrowserHandlerLayeredLogger browserHandler;
 
-    @Before
+    @BeforeEach
     public void instanceSetup() {
         final ExtentTestLoggerFactory extentTestLoggerFactory = ExtentTestLoggerFactoryManager.TEST_REPORT_DIRECTORY.getExtentTestLoggerFactory();
         final WebDriver driver = BrowserHandlerFactory.CHROME.getDriver();
@@ -51,7 +48,7 @@ public class TestSiteTraversal {
         browserHandler
                 .getOptions()
                 .SCREENSHOT_ON_EVENT
-                .setValue(true);
+                .setValue(false);
 
         this.browserHandler = browserHandler;
         this.extentTestLoggerFactory = extentTestLoggerFactory;
@@ -77,8 +74,9 @@ public class TestSiteTraversal {
         }
     }
 
-    @After
-    public void tearDown() {
+    @AfterAll
+    public static void tearDown() {
+        final ExtentTestLoggerFactory extentTestLoggerFactory = ExtentTestLoggerFactoryManager.TEST_REPORT_DIRECTORY.getExtentTestLoggerFactory();
         final BrowserHandlerInterface tempBrowser = BrowserHandlerFactory.CHROME.getBrowserHandler();
         extentTestLoggerFactory.getExtentHtmlReporter().flush();
         extentTestLoggerFactory.getExtentReports().flush();
